9458c5d9a95a393b6c26208637df6ccd5812078e
Akarsh Satija <akarsh.satija@gmail.com>
2014-12-30 10:57:19 +0530

    init + coming soon

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Write-A-Mail</title>
+</head>
+<body>
+<center>
+<h1>Write-A-Mail</h1><br>
+Something awesome coming soon.
+</center>
+</body>
+</html>
